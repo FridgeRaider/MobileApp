42af1c5b7882cf2e94b9b3447b89c98c8c048c50
SergBarrio <serg.barrio@gmail.com>
2013-04-25 14:48:06 -0500

    Added security and some CSS
    utils.js now has some security functions that set cookies on login and
    check for a valid login inside main.html
    
    Also main.html suppresses mobile buttons on larger resolutions and the
    buttons display properly in mobile.

--- phonegap/com.simonmacdonald.scan.BarcodeScanner/assets/www/js/utils.js
+++ phonegap/com.simonmacdonald.scan.BarcodeScanner/assets/www/js/utils.js
@@ -1,3 +1,40 @@
+function setCookie(c_name,value,exdays) {
+	var exdate=new Date();
+	exdate.setDate(exdate.getDate() + exdays);
+	var c_value=escape(value) + ((exdays==null) ? "" : "; expires="+exdate.toUTCString());
+	document.cookie=c_name + "=" + c_value;
+}
+
+function getCookie(c_name) {
+	var c_value = document.cookie;
+	var c_start = c_value.indexOf(" " + c_name + "=");
+	if (c_start == -1) {
+		c_start = c_value.indexOf(c_name + "=");
+		}
+	if (c_start == -1) {
+		c_value = null;
+		}
+	else {
+		c_start = c_value.indexOf("=", c_start) + 1;
+		var c_end = c_value.indexOf(";", c_start);
+		if (c_end == -1) {
+			c_end = c_value.length;
+		}
+		c_value = unescape(c_value.substring(c_start,c_end));
+	}
+	return c_value;
+}
+
+function checkLogin() {
+	var valid = getCookie("validLogin");
+	if (valid != "true") {
+		window.location.href = "index.html";
+	}
+	else {
+		//alert("Good login!");
+	}
+}
+
 function formcenter() {
 	var center = $("#formcenter");
 	center.height( window.innerHeight - 20 );
@@ -18,9 +55,13 @@ function logInResponse(data){
 	console.log(data);
 	
 	var curdate = new Date();
-	if ( (curdate > Date.parse(data.dateJoined)) && data.dateJoined !== undefined){
+	if ( (curdate > Date.parse(data.dateJoined)) && data.dateJoined !== undefined){ 
+		//successful login
+		setCookie("validLogin", "true", 1);
 		window.location.href = "main.html";
 	} else {
+		//unsuccessful login
 		alert("Not a valid email or password!");
 	}
 }
+
